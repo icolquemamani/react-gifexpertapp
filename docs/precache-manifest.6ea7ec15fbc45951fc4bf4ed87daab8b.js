1671d6ab91c4413b82c75c9aa6498ee798817e32
Israel Cm <isracat.32@gmail.com>
2021-05-05 18:16:14 -0400

    add docs folder

---
+++ docs/precache-manifest.6ea7ec15fbc45951fc4bf4ed87daab8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53c926ed0685bc2af99f34200143b37f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b422545543f523e61048",
+    "url": "/static/css/main.79899e5a.chunk.css"
+  },
+  {
+    "revision": "a97792b1af7cd5573739",
+    "url": "/static/js/2.78739b77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78739b77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b422545543f523e61048",
+    "url": "/static/js/main.f8df19ab.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
